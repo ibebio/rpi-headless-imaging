05fe63411e1fd3c07d8d819fc21585ef2e041a69
Ilja Bezrukov <ilja.bezrukov@tuebingen.mpg.de>
2018-06-12 13:53:11 +0200

    Fixed typo

--- code/rpi/root/transfer_images.sh
+++ code/rpi/root/transfer_images.sh
@@ -5,7 +5,7 @@ case "$ACTION" in
 	mount  /dev/sda1 /mnt/
 	mkdir -p /mnt/$(hostname)
 	mv /home/pi/images/*.png /mnt/$(hostname)
-	echo "$(hostname) $(date +%F_%H:%M) >> /mnt/$(hostname)/copylog.txt
+	echo "$(hostname) $(date +%F_%H:%M)" >> /mnt/$(hostname)/copylog.txt
 	mkdir -p /mnt/$(hostname)/syslog/
 	cp /var/log/syslog /mnt/$(hostname)/syslog/syslog.$(date +%F_%H:%M)
 	umount /mnt/
